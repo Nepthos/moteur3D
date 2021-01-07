ef04d44c8d57fb4efd14a2f32cdd9295b8862468
Akahiro54 <adrien.palmieri9@etu.univ-lorraine.fr>
2021-01-07 10:38:08 +0100

    Added base project

---
+++ Moteur3D/cmake-build-debug/CMakeFiles/Moteur3D.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Moteur3D.dir/main.cpp.o"
+  "Moteur3D"
+  "Moteur3D.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Moteur3D.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
